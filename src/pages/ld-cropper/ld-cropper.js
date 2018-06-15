47cbb5f23a366da54808bf4e9c9c76d2fe750d95
liujia <liujia30@jd.com>
2018-06-15 15:49:58 +0800

    abc

--- src/pages/ld-cropper/ld-cropper.js
+++ src/pages/ld-cropper/ld-cropper.js
@@ -317,7 +317,7 @@ Page({
   onShareAppMessage: function () {
     return {
       path: '/pages/index/index',
-      title: '听说你会换头像？',
+      title: '一起换个618头像，给京东618打call吧',
       imageUrl: 'https://img10.360buyimg.com/uba/jfs/t22567/88/33870292/39514/82a70213/5b235b76Nf0bc188b.jpg'
     }
   }
