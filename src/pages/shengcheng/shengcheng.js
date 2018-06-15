c8bd50d9ba4f7ffdb384bd3dddb582f072dc5034
unknown <zhoupengfei@ZB-81v5ym2.360buyAD.local>
2018-06-15 13:47:00 +0800

    啊实打实地方

---
+++ src/pages/shengcheng/shengcheng.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    // 用户头像
+    userInfoimg: 'http://img14.360buyimg.com/cms/jfs/t23863/203/35300054/215678/7557322e/5b234f70Ndd444df0.png'
+  },
+  onLoad: function () {
+  },
+  onShow: function () {
+    
+  }
+})
